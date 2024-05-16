fcbe173081868a5ff7be5e0d75f6ff033e8ce5a3
Nivek <41105006+44nivi@users.noreply.github.com>
2024-05-16 11:47:43 +0530

    Add files via upload
    design patterns

---
+++ design_patterns/src/Factory_Method/Factory.java
@@ -0,0 +1,30 @@
+package Factory_Method;
+
+public class Factory {
+	public interface Shape {
+		   void draw();
+		}
+	
+	public class Rectangle implements Shape{
+
+		@Override
+		public void draw() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
+	public class Square implements Shape{
+
+		@Override
+		public void draw() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	
+	
+}
---
+++ design_patterns/src/singleton1/Singleton_Main.java
@@ -0,0 +1,9 @@
+package singleton1;
+
+public class Singleton_Main {
+
+	public static void main(String[] args) {
+	      Singleton tmp = Singleton.getInstance();
+	      tmp.demoMethod();
+	   }
+}
